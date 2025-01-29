99b90ec446ca281b152997663d5f426f5cf1874b
Angela Schumacher <angelaschumacher@web.de>
2025-01-29 15:11:13 +0100

    added second recursive solution

--- script.js
+++ script.js
@@ -20,6 +20,8 @@ const checkUserInput = () => {
     return;
   }
   let input = number.value;
+    
+
   let romanNumeral = "";
   const numbers = {
     M: 1000,
@@ -37,16 +39,46 @@ const checkUserInput = () => {
     I: 1,
   };
 
-  for (let i of Object.keys(numbers)) {
-    let q = Math.floor(input / numbers[i]);
-    input -= q * numbers[i];
-    romanNumeral += i.repeat(q);
-  }
+for (let i of Object.keys(numbers)) {
+  let q = Math.floor(input / numbers[i]);
+  input -= q * numbers[i];
+  romanNumeral += i.repeat(q);
+}
 
-  output.innerText = romanNumeral;
+output.innerText = romanNumeral;
+
+/*
+let numbers = [
+  [1000, 'M'],
+  [900, 'CM'],
+  [500, 'D'],
+  [400, 'CD'],
+  [100, 'C'],
+  [90, 'XC'],
+  [50, 'L'],
+  [40, 'XL'],
+  [10, 'X'],
+  [9, 'IX'],
+  [5, 'V'],
+  [4, 'IV'],
+  [1, 'I']
+]
+
+const convert = (input) => {
+  if (input === 0) return "";
+  for(let i = 0; i < numbers.length; i++) {
+    if (input >= numbers[i][0]) return numbers[i][1] + convert(input - numbers[i][0]);
+  }
+}
+  
+
+output.innerText = convert(input);
+*/
+
   output.style.display = "block";
   number.value = "";
   return;
+
 };
 
 //event listeners
