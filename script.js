620c258e1943217abd1f8d37d7dfbb0b480a5b10
Angela Schumacher <angelaschumacher@web.de>
2025-01-31 12:36:47 +0100

    practice session, no changes

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ const checkUserInput = () => {
     return;
   }
   let input = number.value;
-    
-
+  
+  /*
   let romanNumeral = "";
   const numbers = {
     M: 1000,
@@ -39,41 +39,43 @@ const checkUserInput = () => {
     I: 1,
   };
 
-for (let i of Object.keys(numbers)) {
-  let q = Math.floor(input / numbers[i]);
-  input -= q * numbers[i];
-  romanNumeral += i.repeat(q);
-}
-
-output.innerText = romanNumeral;
-
-/*
-let numbers = [
-  [1000, 'M'],
-  [900, 'CM'],
-  [500, 'D'],
-  [400, 'CD'],
-  [100, 'C'],
-  [90, 'XC'],
-  [50, 'L'],
-  [40, 'XL'],
-  [10, 'X'],
-  [9, 'IX'],
-  [5, 'V'],
-  [4, 'IV'],
-  [1, 'I']
-]
-
-const convert = (input) => {
-  if (input === 0) return "";
-  for(let i = 0; i < numbers.length; i++) {
-    if (input >= numbers[i][0]) return numbers[i][1] + convert(input - numbers[i][0]);
+  for (let i of Object.keys(numbers)) {
+    let q = Math.floor(input / numbers[i]);
+    input -= numbers[i] * q;
+    romanNumeral += i.repeat(q);
   }
-}
   
+  output.textContent = romanNumeral;
+ */
+
+  let numbers = [
+    [1000, 'M'],
+    [900, 'CM'],
+    [500, 'D'],
+    [400, 'CD'],
+    [100, 'C'],
+    [90, 'XC'],
+    [50, 'L'],
+    [40, 'XL'],
+    [10, 'X'],
+    [9, 'IX'],
+    [5, 'V'],
+    [4, 'IV'],
+    [1, 'I']
+  ]
+
+ const convert = (input) => {
+  if (input === 0) return "";
+  for (let i = 0; i < numbers.length; i++) {
+    if (input >= numbers[i][0]) {
+      return numbers[i][1] + convert(input - numbers[i][0]);
+    }
+  }
+ }
+
 
 output.innerText = convert(input);
-*/
+
 
   output.style.display = "block";
   number.value = "";
